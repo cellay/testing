9d415fd8a15b5e27b3f8c65757f152977a8b7ada
cellay <camposellay@gmail.com>
2018-01-10 05:03:55 -0500

    5am

--- js/app.js
+++ js/app.js
@@ -9,8 +9,11 @@ var config = {
 };
 firebase.initializeApp(config);
 
+var user = null;
+
 // Uniendo Firebase register
 $('#register').on('click', function (event) {
+  console.log('hey');
   var email = $('#email').val();
   var password = $('#pass').val();
   firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
@@ -18,12 +21,13 @@ $('#register').on('click', function (event) {
     var errorCode = error.code;
     var errorMessage = error.message;
     // ...
-    $(location).attr('href', 'newsfeed.html');
+    
   });
 });
 
 //Uniendo Firebase login
 $('#login').on('click', function (event) {
+  console.log('hey2');
   var email = $('#email-register').val();
   var password = $('#pass-register').val();
   firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
@@ -31,18 +35,64 @@ $('#login').on('click', function (event) {
     var errorCode = error.code;
     var errorMessage = error.message;
     // ...
-    $(location).attr('href', 'newsfeed.html');
-  });
+    $(location).attr('href', 'views/newsfeed.html');
+  }); 
 
-//   var user = firebase.auth().currentUser;
-//   if (user) {
-//     // User is signed in.
-//     ;
-//   } else {
-//     // No user is signed in.
-//     alert('Ingresa o regístrate');
-//   }
 });
 
+function login(event) {
+  var user = firebase.auth().currentUser;
+  if (user) {
+    // User is signed in.
+    ;
+  } else {
+    // No user is signed in.
+    alert('Ingresa o regístrate');
+  }
+};
+
+// Inicio de sesión con Google y Facebook
+
+$('#login-fb').on('click', function(event) {
+  console.log('KHE?');
+  var provider = new firebase.auth.FacebookAuthProvider();
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    var token = result.credential.accessToken;
+    var user = result.user;
+    // ...
+    $(location).attr('href', 'views/newsfeed.html'); //Direccionamos a la view de newsfeed
+
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+
+});
+
+$('#login-google').on('click', function(event) {
+  var provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    var token = result.credential.accessToken;
+    var user = result.user;
+    // ...
+    $(location).attr('href', 'views/newsfeed.html'); //Direccionamos a la view de newsfeed
+    
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+});
 
 
